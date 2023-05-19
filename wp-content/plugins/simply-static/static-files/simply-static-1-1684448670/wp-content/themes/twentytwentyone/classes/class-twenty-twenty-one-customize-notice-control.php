0d03d09a6990676634ec84ea494ab8c9d71bdff6
Juan Carlos Hernández <juan_hdezc@outlook.es>
2023-05-18 18:11:26 -0600

    reparacion de footer margen

--- wp-content/plugins/simply-static/static-files/simply-static-1-1684448670/wp-content/themes/twentytwentyone/classes/class-twenty-twenty-one-customize-notice-control.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Class &quot;WP_Customize_Control&quot; not found in D:\xampp\htdocs\wordpress\wp-content\themes\twentytwentyone\classes\class-twenty-twenty-one-customize-notice-control.php:17
-Stack trace:
-#0 {main}
-  thrown in <b>D:\xampp\htdocs\wordpress\wp-content\themes\twentytwentyone\classes\class-twenty-twenty-one-customize-notice-control.php</b> on line <b>17</b><br>
